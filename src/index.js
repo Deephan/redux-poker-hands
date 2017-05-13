3866a8138b3fa136748e7cf2df5317792b6c049c
Deephan <prototypedeployer@gmail.com>
2017-05-13 19:19:25 -0400

    chore: Working version
    Committing the crude working version without any “redux”ing and result validation.
    Roll back to this revision in case of any mishaps in the future.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
+import DealArea from './components/DealArea'
+import ButtonsPanel from './components/ButtonsPanel'
+
+// NOTE TO SELF: App would be the main container! The poker panel will
+//               eventually be rendered from App!
+
+/*
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+*/
+
+ReactDOM.render(
+  <DealArea/>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ButtonsPanel/>,
+  document.getElementById('answer')
 );
